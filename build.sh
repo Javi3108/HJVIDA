03b18c1ae0e9c00bb87fc201785940d2135bc6c8
Javier <javierapo152@gmail.com>
2026-01-28 00:04:03 -0500

    Cambio de superusuario a Alejandro

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Crear/Asegurar superusuario
+# Crear/Asegurar superusuario con TUS DATOS
 echo "Asegurando superusuario..."
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
-User.objects.filter(username='Valeria').exists() or \
-User.objects.create_superuser('Valeria', 'Val@gmail.com', 'Nam123')" \
+User.objects.filter(username='Alejandro').exists() or \
+User.objects.create_superuser('Alejandro', 'tu-correo@gmail.com', 'TuClaveSegura123')" \
 | python manage.py shell
